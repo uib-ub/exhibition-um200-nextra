70b47beecd1e7a63e15a4c57d68736d2a2f367d0
Tarje Lavik <tarje.lavik@gmail.com>
2025-10-30 21:54:27 +0100

    Enhance Card component styles: adjust image border radius for better responsiveness, update title size for improved readability, and conditionally render description. Modify Cards layout for consistent grid structure and spacing.

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -14,14 +14,14 @@ interface CardProps {
 export function Card({ title, description, image, imageAlt, href }: CardProps) {
   return (
     <CardComponent className="border-none shadow-none p-0">
-      <CardContent className="p-0 flex flex-col justify-start gap-2">
+      <CardContent className="p-0 flex flex-col justify-start gap-5">
         <Link href={href} >
-          <Image src={image} alt={imageAlt} width={100} height={100} className="w-full aspect-square object-cover rounded-t-[60px]" />
+          <Image src={image} alt={imageAlt} width={100} height={100} className="w-full aspect-square object-cover rounded-t-[30px] sm:rounded-t-[50px] md:rounded-t-[60px]" />
         </Link>
         <Link href={href}>
-          <CardTitle className="text-xl md:text-3xl">{title}</CardTitle>
+          <CardTitle className="text-md md:text-xl">{title}</CardTitle>
         </Link>
-        <CardDescription className="text-sm md:text-base">{description}</CardDescription>
+        {description ? <CardDescription className="text-sm md:text-base">{description}</CardDescription> : null}
         <Button variant="outline" className="w-min border-black" asChild>
           <Link href={href}>Les mer</Link>
         </Button>
@@ -33,8 +33,8 @@ export function Card({ title, description, image, imageAlt, href }: CardProps) {
 export function Cards({ title, children }: { title: string, children: React.ReactNode }) {
   return (
     <>
-      <h2 className="feature text-2xl font-bold py-5">{title}</h2>
-      <div className="feature grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 w-full">
+      <h2 className="feature text-2xl font-bold pt-5">{title}</h2>
+      <div className="feature grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 w-full">
         {children}
       </div>
     </>
