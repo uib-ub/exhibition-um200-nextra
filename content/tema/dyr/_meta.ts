129dba3021bb4ec8a6a0c3e2127b468320fc36e7
Tarje Lavik <tarje.lavik@gmail.com>
2025-11-03 11:42:28 +0100

    Demonstration of asIndexPage creating link from sidebar dropdown level.

--- content/tema/dyr/_meta.ts
+++ content/tema/dyr/_meta.ts
@@ -1,10 +1,25 @@
 const meta = {
-  'index': {
-    type: 'page',
-    theme: {
-      layout: 'full',
-      toc: false
-    }
+  /*  'index': {
+     type: 'page',
+     theme: {
+       layout: 'full',
+       toc: false
+     }
+   }, */
+  'bucher-og-blaakvalen': {
+    type: 'doc',
+  },
+  'james-grieg': {
+    type: 'doc',
+  },
+  'kaffe-og-elskov-i-blaahvalens-buk': {
+    type: 'doc',
+  },
+  'plastkvalen': {
+    type: 'doc',
+  },
+  'kvalsalen': {
+    type: 'doc',
   },
 }
 
