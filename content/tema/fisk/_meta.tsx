423b86b93250a1c41b28783c4aae03aa3a2a3e4b
Tarje Lavik <tarje.lavik@gmail.com>
2025-11-06 08:19:02 +0100

    Update theme metadata for 'Fisk': replace old _meta.ts with new _meta.tsx, adding icon support and improving layout for better presentation.

---
+++ content/tema/fisk/_meta.tsx
@@ -0,0 +1,18 @@
+import { IconFish } from '@tabler/icons-react';
+
+const meta = {
+  'root_link': {
+    title: (
+      <div className="flex flex-row items-center gap-1 justify-self-stretch text-md md:text-xl">
+        <IconFish className="w-6 h-6 md:w-8 md:h-8 hidden md:inline-block" />
+        <div>Fisk</div>
+      </div>
+    ),
+    href: '/tema/fisk',
+  },
+  'christie-og-de-maerkelige-fiske': {
+    type: 'doc',
+  },
+}
+
+export default meta;
