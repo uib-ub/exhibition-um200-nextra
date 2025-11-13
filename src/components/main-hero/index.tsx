e7eb1ad1e122b2bdae1aea5de60bd7c36b1c4bdd
Tarje Lavik <tarje.lavik@gmail.com>
2025-11-13 13:52:40 +0100

    Refactor MainHero component: update layout and styling for improved readability and visual hierarchy. Adjust grid structure, enhance typography, and modify spacing for better alignment.

--- src/components/main-hero/index.tsx
+++ src/components/main-hero/index.tsx
@@ -1,20 +1,22 @@
 import Image from 'next/image'
 import illustration from '@/components/main-hero/main-illustration.png'
-import logo from '@/components/main-hero/uib-logo.svg'
 
 export function MainHero() {
   return (
-    <div className="relative flex flex-col gap-10 items-center justify-center w-full lg:max-w-7xl mx-auto">
-      <div className="grid grid-cols-[200px_1fr_200px] gap-5 items-center justify-center w-full">
-        <Image src={logo} alt="Universitetet i Bergen" width={160} height={160} className="justify-self-start" />
-        <h1 className="text-2xl md:text-4xl text-center uppercase"><span className='text-xl md:text-2xl'>200 år med</span><br />vidunderlig vitenskap</h1>
-        <div>{' '}</div>
+    <div className="relative flex flex-col gap-10 items-center justify-center w-full lg:max-w-7xl mx-auto pt-8">
+      <div className="grid grid-cols-1 gap-5 items-center justify-center w-full">
+        <h1 className="text-2xl md:text-4xl text-center uppercase">
+          <span className='text-lg md:text-xl md:pb-3 inline-block'>200 år med</span>
+          <br />
+          vidunderlig vitenskap
+        </h1>
+
       </div>
       <Image src={illustration} alt="200 år med vidunderlig vitenskap" width={1200} height={1000} className="w-full" />
-      <div className="flex flex-row gap-5 items-center justify-between w-3/4">
-        <p className="text-xl aspect-square flex items-center justify-center font-bold md:text-2xl rounded-full bg-um-arkeologi-solid text-background px-4 py-2">1825</p>
-        <p className="text-xl md:text-2xl text-center">Universitetet i Bergen, Universitetsmuseet og Universitetsbiblioteket</p>
-        <p className="text-xl aspect-square flex items-center justify-center font-bold md:text-2xl rounded-full bg-um-arkeologi-solid text-background px-4 py-2  ">2025</p>
+      <div className="flex flex-row gap-5 items-center justify-between w-full">
+        <p className="text-xl aspect-square flex items-center justify-center font-medium md:text-2xl ">1825</p>
+        <p className="text-xl md:text-2xl text-center">Universitetet i Bergen, Universitetsmuseet<br />& Universitetsbiblioteket</p>
+        <p className="text-xl aspect-square flex items-center justify-center font-medium md:text-2xl ">2025</p>
       </div>
     </div>
   )
