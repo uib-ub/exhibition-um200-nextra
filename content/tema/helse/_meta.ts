d37528c43fbc63bd47e1723acf706d957b64240c
Tarje Lavik <tarje.lavik@gmail.com>
2025-11-21 15:34:29 +0100

    Replace _meta.ts with _meta.tsx for the 'Helse' theme, enhancing the metadata structure with a JSX title that includes an icon. Update index.mdx to include the JumpToStories component and restructure the Cards section for improved layout and navigation.

--- content/tema/helse/_meta.ts
+++
@@ -1,8 +0,0 @@
-const meta = {
-  'root_link': {
-    title: 'Helse',
-    href: '/tema/helse',
-  },
-}
-
-export default meta;
---
+++ content/tema/helse/_meta.tsx
@@ -0,0 +1,15 @@
+import { IconHeart } from '@tabler/icons-react';
+
+const meta = {
+  'root_link': {
+    title: (
+      <div className="flex flex-row items-center gap-1 justify-self-stretch text-md md:text-xl">
+        <IconHeart className="w-5 h-5 inline-block" />
+        <div>Helse</div>
+      </div>
+    ),
+    href: '/tema/helse',
+  },
+}
+
+export default meta;
