2370438d8cfe34d0ff2360bed9af2f7423807498
Tarje Lavik <tarje.lavik@gmail.com>
2025-11-21 15:44:49 +0100

    Refactor 'Vann, vær og vind' theme by replacing _meta.ts with _meta.tsx, enhancing the metadata structure with a JSX title that includes an icon. Update index.mdx to include a JumpToStories component and restructure the Cards section for improved layout and navigation.

---
+++ content/tema/vann-vaer-og-vind/_meta.tsx
@@ -0,0 +1,15 @@
+import { IconCloudRain } from '@tabler/icons-react';
+
+const meta = {
+  'root_link': {
+    title: (
+      <div className="flex flex-row items-center gap-1 justify-self-stretch text-md md:text-xl">
+        <IconCloudRain className="w-5 h-5 inline-block" />
+        <div>Vann, vær og vind</div>
+      </div>
+    ),
+    href: '/tema/vann-vaer-og-vind',
+  },
+}
+
+export default meta;
