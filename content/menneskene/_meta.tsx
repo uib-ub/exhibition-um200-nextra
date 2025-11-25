8ef5d44b211d6a4b733f1d5228148555d288deb6
Tarje Lavik <tarje.lavik@gmail.com>
2025-11-25 20:07:17 +0100

    Add new pages and metadata for notable figures in Menneskene section

---
+++ content/menneskene/_meta.tsx
@@ -0,0 +1,27 @@
+const meta = {
+  'index': {
+    type: 'page',
+    theme: {
+      layout: 'full',
+      toc: false,
+    }
+  },
+  'wfk-christie': {
+    type: 'page',
+    theme: {
+      layout: 'full',
+      toc: false,
+      breadcrumb: true,
+    }
+  },
+  'olaf-hanssen': {
+    type: 'page',
+    theme: {
+      layout: 'full',
+      toc: false,
+      breadcrumb: true,
+    }
+  },
+}
+
+export default meta;
