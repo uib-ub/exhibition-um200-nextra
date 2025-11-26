c962ec0bd68fa76efe43913fa638be42faf4da29
Tarje Lavik <tarje.lavik@gmail.com>
2025-11-26 10:25:09 +0100

    Add additional resource links to the meta file for Samisk samling, Museets første gjenstand, Wayang dukketeater, and Admiral Bruni D’Entrecasteauxs samling.

--- content/tema/verden-i-bergen/_meta.tsx
+++ content/tema/verden-i-bergen/_meta.tsx
@@ -10,6 +10,22 @@ const meta = {
     ),
     href: '/tema/verden-i-bergen',
   },
+  'samisk-samling_link': {
+    title: 'Samisk samling',
+    href: 'https://samisksamling.w.uib.no/',
+  },
+  'museets-forste-gjenstand_link': {
+    title: 'Museets første gjenstand?',
+    href: 'https://tohundrefortellinger.w.uib.no/1825/09/26/1827-en-kinesisk-pagode/',
+  },
+  'wayang-dukketeater-fra-java_link': {
+    title: 'Wayang - dukketeater fra Java',
+    href: 'https://tohundrefortellinger.w.uib.no/1970/09/18/1970-wayang-dukketeater-fra-java/',
+  },
+  'admiral-bruni-dentrecasteauxs-samling-fra-stillehavet_link': {
+    title: 'Admiral Bruni D’Entrecasteauxs samling fra Stillehavet',
+    href: 'https://tohundrefortellinger.w.uib.no/1970/09/18/1970-wayang-dukketeater-fra-java/',
+  },
 }
 
 export default meta;
