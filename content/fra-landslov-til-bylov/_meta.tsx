4a13f43a82a6922667d9f3a28168d06399ca7409
Robert K. Paulsen <robert.paulsen@uib.no>
2026-01-16 13:52:53 +0100

    add more subdivs

---
+++ content/fra-landslov-til-bylov/_meta.tsx
@@ -0,0 +1,12 @@
+const meta = {
+    '*': {
+        //type: 'page',
+        theme: {
+            layout: 'full',
+            toc: true,
+            breadcrumb: true
+        }
+    }
+}
+
+export default meta;
